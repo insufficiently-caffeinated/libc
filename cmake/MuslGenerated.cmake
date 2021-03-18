f44e21c5f94d5aecdb12bf8ad61916a7f71d8518
Phantomical <phantom@lynches.ca>
2021-03-18 00:57:53 -0400

    Replace CMAKE_SOURCE_DIR -> CMAKE_CURRENT_SOURCE_DIR

--- cmake/MuslGenerated.cmake
+++ cmake/MuslGenerated.cmake
@@ -5,8 +5,8 @@ make_directory("${CMAKE_BINARY_DIR}/include/internal")
 add_custom_command(
   OUTPUT "${CMAKE_BINARY_DIR}/include/bits/alltypes.h"
   COMMAND sh ARGS -c "sed -f tools/mkalltypes.sed arch/${MUSL_ARCH}/bits/alltypes.h.in include/alltypes.h.in > '${CMAKE_BINARY_DIR}/include/bits/alltypes.h'"
-  DEPENDS "${CMAKE_SOURCE_DIR}/VERSION" "${CMAKE_SOURCE_DIR}/.git"
-  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+  DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/VERSION" "${CMAKE_CURRENT_SOURCE_DIR}/.git"
+  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
   VERBATIM
 )
 
@@ -14,15 +14,15 @@ add_custom_command(
   OUTPUT "${CMAKE_BINARY_DIR}/include/bits/syscall.h"
   COMMAND sh ARGS -c "cp \"arch/${MUSL_ARCH}/bits/syscall.h.in\" \"${CMAKE_BINARY_DIR}/include/bits/syscall.h\"; sed -n -e s/__NR_/SYS_/p < \"arch/${MUSL_ARCH}/bits/syscall.h.in\" >> \"${CMAKE_BINARY_DIR}/include/bits/syscall.h\""
   DEPENDS "arch/${MUSL_ARCH}/bits/syscall.h.in"
-  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
   VERBATIM
 )
 
 add_custom_command(
   OUTPUT "${CMAKE_BINARY_DIR}/include/internal/version.h"
   COMMAND sh ARGS -c "sh tools/version.sh > \"${CMAKE_BINARY_DIR}/include/internal/version.h\""
-  DEPENDS "${CMAKE_SOURCE_DIR}/VERSION" "${CMAKE_SOURCE_DIR}/.git"
-  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+  DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/VERSION" "${CMAKE_CURRENT_SOURCE_DIR}/.git"
+  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
   VERBATIM
 )
 
